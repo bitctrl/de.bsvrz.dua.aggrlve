d3af1b4119f9596ce7c0e5904c71411f002bf4d6
tfelder <info@bitctrl.de>
2008-03-06 09:02:48 +0000

    Kappich PrüfSpez 2.7.1
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.aggrlve@7237 40c0be50-5d29-0410-901d-fb59edb5cafc

--- junit/de/bsvrz/dua/aggrlve/Verbindung.java
+++ junit/de/bsvrz/dua/aggrlve/Verbindung.java
@@ -47,7 +47,8 @@ public class Verbindung {
 	/**
 	 * Wurzelverzeichnis der Testdaten z.Z. <code>V_2.1(11.12.07)</code>
 	 */
-	public static final String WURZEL = ".\\extra\\testDaten\\V_2.7(20.02.08)\\"; //$NON-NLS-1$
+	public static final String WURZEL = ".\\extra\\testDaten\\V_2.7.1(05.03.08)\\"; //$NON-NLS-1$
+//	public static final String WURZEL = ".\\extra\\testDaten\\V_2.7(20.02.08)\\"; //$NON-NLS-1$
 	
 	
 	/**
