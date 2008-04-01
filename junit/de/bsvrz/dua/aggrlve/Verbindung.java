bf3904eaaed7dcc565b31fca657e0064716afbe2
tfelder <info@bitctrl.de>
2008-04-01 08:02:45 +0000

    Neue Testdaten
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.aggrlve@7832 40c0be50-5d29-0410-901d-fb59edb5cafc

--- junit/de/bsvrz/dua/aggrlve/Verbindung.java
+++ junit/de/bsvrz/dua/aggrlve/Verbindung.java
@@ -45,11 +45,24 @@ public class Verbindung {
 		"-authentifizierung=passwd",  //$NON-NLS-1$
 		"-debugLevelStdErrText=WARNING", //$NON-NLS-1$
 		"-debugLevelFileText=WARNING"}; //$NON-NLS-1$
-	
+
+//	/**
+//	 * Verbindungsdaten fuer Eclipse
+//	 */
+//	private static final String[] CON_DATA = new String[] {
+//		"-datenverteiler=localhost:8083",   //$NON-NLS-1$
+//		"-benutzer=Tester",  //$NON-NLS-1$
+//		"-authentifizierung=c:\\passwd",  //$NON-NLS-1$
+//		"-debugLevelStdErrText=WARNING", //$NON-NLS-1$
+//		"-debugLevelFileText=WARNING"}; //$NON-NLS-1$
+
 	/**
 	 * Wurzelverzeichnis der Testdaten
 	 */
-	public static final String WURZEL = ".." + File.separator + "testDaten" + File.separator + "V_2.7.3(20.03.08)" + File.separator; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+//	public static final String WURZEL = "extra\\testDaten\\V_2.7.6(01.04.08)" + File.separator; //$NON-NLS-1$
+
+	public static final String WURZEL = ".." + File.separator + "testDaten" + File.separator + "V_2.7.6(01.04.08)" + File.separator; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+//	public static final String WURZEL = ".." + File.separator + "testDaten" + File.separator + "V_2.7.3(20.03.08)" + File.separator; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 //	public static final String WURZEL = ".." + File.separator + "testDaten" + File.separator + "V_2.7.2(12.03.08)" + File.separator; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 //	public static final String WURZEL = ".." + File.separator + "testDaten" + File.separator + "V_2.7.1(05.03.08)" + File.separator; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	
